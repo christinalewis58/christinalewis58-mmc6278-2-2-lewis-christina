6dd7d861fb5141a04cacbed6dc88a72b93201ac1
Christina Lewis <clewis13@ufl.edu>
2022-05-22 20:54:24 -0400

    ask how old and parseint

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function greet(username) {
     var username = prompt("What is your name?");
     alert("Hello, " + username + ".");
+    var userAge = prompt("How old are you?");
+    var userAge = parseInt(userAge);
 }
